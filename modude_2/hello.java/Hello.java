22867ec6056d21bc33150a6c773ed839e4083e79
unknown <dangquank51@gmail.com>
2021-06-18 20:12:09 +0700

    update

---
+++ modude_2/hello.java/Hello.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+/**
+ * Hello
+ */
+public class Hello {
+ public static void main(String[] args) {
+     System.out.println("hello baby!");
+
+     Date today = new Date();
+     System.out.println("Today is " + today);
+ }
+    
+}
