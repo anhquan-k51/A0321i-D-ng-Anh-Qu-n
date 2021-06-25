3853e063bea6e38c0cb1537f9cc0f57c4f630b40
unknown <dangquank51@gmail.com>
2021-06-25 19:20:02 +0700

    now

---
+++ modude_2/Mang_Trong_java/bai_tap/xoa_phan_tu.java
@@ -0,0 +1,58 @@
+
+import java.util.Scanner;
+
+public class xoa_phan_tu {
+    
+	public static void ramdom(int a[],int n) {
+		int i;
+		for(i = 0; i < n; i++)
+			a[i] = (int) (Math.random() * 100);
+	}
+	
+	public static void out(int a[],int n) {
+		int i;
+		for(i = 0; i < n; i++)
+			System.out.print(a[i]+" ");
+	}
+	
+	public static void xoatPt(int a[],int n,int k)
+	{	
+		int i,c,index;
+		boolean check = false;
+		for (c = i = 0; i < n; i++) {
+	        if (a[i] != k) {
+	            a[c] = a[i];
+	            c++;
+	            check = true;
+	        }
+	      
+	    }
+		
+		n = c;
+		if(check) {
+			System.out.print("Mang sau khi xoa\n");
+			out(a, n);
+		}		
+		else 
+			System.out.print("\nKhong tim thay "+k+" trong mang");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap so phan tu:");
+		int n = sc.nextInt();
+		int []a = new int [n];
+		
+		System.out.print("Mang ramdom la\n");
+		ramdom(a, n);
+		out(a, n);
+
+		System.out.print("\nNhap gia tri muon xoa:");
+		int k = sc.nextInt();
+		
+		xoatPt(a, n, k);
+		
+}
+}
