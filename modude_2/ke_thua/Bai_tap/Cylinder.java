5ef948bbfb68f7d450eb009736bf031b007ed439
unknown <dangquank51@gmail.com>
2021-07-09 18:43:06 +0700

    update

---
+++ modude_2/ke_thua/Bai_tap/Cylinder.java
@@ -0,0 +1,76 @@
+package ke_thua.Bai_tap;
+
+public class Cylinder extends Circle {
+	
+	private double height = 1.0;
+	
+	public Cylinder() {
+		
+	}
+	
+	public Cylinder(double height) {
+		this.height = height;
+	}
+	
+	public Cylinder(double radius , String color , double height) {
+		super(radius,color);
+		this.height = height;
+	}
+	
+	public double getHeight () {
+		return this.height;
+	}
+	
+	public void setHeight(double h) {
+		this.height = h;
+	}
+	
+	public double getVolume() {
+		return getArea() * this.height;
+	}
+	
+	
+	
+	public String toString() {
+		return "A Cylinder with height = " + getHeight() + 
+				", which is a subclass of " + super.toString() + 
+				" and volume = " + getVolume() + " m3";
+	}
+}
+
+class Circle {
+	private double radius;
+	private String color = "yellow";
+	
+	public Circle() {
+    }
+
+    public Circle(double radius,String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+    
+    public double getRadius() {
+        return radius;
+    }
+    
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    
+    public String getColor() {
+    	return this.color;
+    }
+    
+    public void setColor(String color) {
+    	this.color = color;
+    }
+    
+    public double getArea() {
+        return this.radius*this.radius*Math.PI;
+    }
+    
+    public String toString() {
+    	return "A Circle with color of "+ getColor() + " ,radius = " + getRadius() + " m" ;
+    }
+}
---
+++ modude_2/ke_thua/Bai_tap/TesterCylinder.java
@@ -0,0 +1,12 @@
+package ke_thua.Bai_tap;
+
+public class TesterCylinder {
+	public static void main(String[] args) {
+		Circle obj = new Circle(2,"red");
+		System.out.println(obj);
+		
+		Cylinder obj1 = new Cylinder(3,"green",5);
+		System.out.println(obj1);
+    }
+}
+	
